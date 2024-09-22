addd4e07a4a4a497bf92d89be194c9d8e2c58e98
Jhonnatan Gomez <jalfgomez@poligran.edu.co>
2024-09-22 18:08:04 -0500

    Se clona el repositorio en entorno local Jhonnatan Gomez, para realizar vista de perfil

--- app/src/main/java/com/example/proyecto_poli/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_poli/MainActivity.kt
@@ -16,32 +16,6 @@ import com.example.proyecto_poli.ui.theme.Proyecto_poliTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Proyecto_poliTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Proyecto_poliTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
