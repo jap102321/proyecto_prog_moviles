29dc756aed82521215bbab2ca87a9486881e5222
Jhonnatan Gomez <jalfgomez@poligran.edu.co>
2024-09-23 01:05:31 -0500

    Se crea vista del perfil

--- app/src/main/java/com/example/proyecto_poli/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_poli/MainActivity.kt
@@ -17,5 +17,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
